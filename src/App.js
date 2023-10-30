5424ecc2ca3ff20ced65433b52dd281b5d44e9bb
Jack Margeson <margeson.jack@gmail.com>
2023-10-30 13:47:28 -0400

    Basic dashboard framework init.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
-  return (
-    <div>
-      <p>Hello, world!</p>
-    </div>
-  );
+	return (
+		<div className="main">
+			<div className="display-container">
+				<div className="sidebar-container">
+					<div className="profile-container">
+						<p>profile comp here</p>
+					</div>
+					<div className="navigation-container">
+						<p>navigation comp here</p>
+					</div>
+				</div>
+				<div className="body-container">
+					<div className="titlebar-container">
+						<p>titlebar comp here</p>
+					</div>
+					<div className="content-container">
+						<p>main content comp here</p>
+					</div>
+				</div>
+			</div>
+			<div className="global-footer-container">
+				<p>
+					<a href="https://github.com/jack-margeson/alterna-canvas">
+						CS5167 User Interface I project #2, Alterna-Canvas.
+					</a>{" "}
+					Built with 💖 by Jack, Kody, Alex, and Al.
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
