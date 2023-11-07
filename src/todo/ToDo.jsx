0da163e3d30fdde16f7fdcf847c3d0d8d5eee6a9
ko-penn <71391703+ko-penn@users.noreply.github.com>
2023-11-06 22:45:07 -0500

    Added content to the main ToDo tab

--- src/todo/ToDo.jsx
+++ src/todo/ToDo.jsx
@@ -1,12 +1,19 @@
 import "./ToDo.scss";
+import TodoItem from "../class-card/TodoItem"
 
 function ToDo({childToParent}) {
     childToParent("Classes");
 
 	return (
-        <>
-            <p>Under Construction</p>
-        </>
+        <div className="to-do-container">
+            <TodoItem text="CS5001 - HW 3"></TodoItem>
+            <TodoItem text="CS5001 - Quiz 3"></TodoItem>
+            <TodoItem text="CS5160 - HW 4a"></TodoItem>
+            <TodoItem text="CS5160 - HW 4b"></TodoItem>
+            <TodoItem text="CS5167 - MON class activity"></TodoItem>
+            <TodoItem text="CS5167 - HW 5"></TodoItem>
+            <TodoItem text="CS5167 - Midterm 1"></TodoItem>
+        </div>
 	);
 }
 
