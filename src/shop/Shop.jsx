4d164b940d7de9ef70ee379b8bce02e34678a300
Jack Margeson <margeson.jack@gmail.com>
2023-11-13 19:30:27 -0500

    Shop rework

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,11 +1,41 @@
+import * as React from "react";
+import { Button, Dialog } from "@mui/material";
 import "./Shop.scss";
 import ShopItem from "./ShopItem";
 
-function Shop({childToParent}) {
+function Shop({ childToParent }) {
     childToParent("Classes");
 
-	return (
-		<div className="shop-main">
+    const [open, setOpen] = React.useState(false);
+
+    const [shopDialogString, setShopDialogString] = React.useState("");
+
+    const handleClose = (value) => {
+        setOpen(false);
+    };
+
+    const handleBuy = () => {
+        const totalPoints = document.getElementById("points");
+        const shopValue = document.getElementById("shop-value");
+
+        if (Number(shopValue.textContent) !== 0) {
+            if (Number(totalPoints.textContent) < Number(shopValue.textContent)) {
+                setShopDialogString("Insufficent point funds! Try earning more points by submitting assignments.")
+            } else {
+                totalPoints.textContent = Number(totalPoints.textContent) - Number(shopValue.textContent);
+                setShopDialogString("Transaction complete! You spent " + shopValue.textContent + " points and now have a total of " + totalPoints.textContent + " points.")
+                let tmpColor = totalPoints.style.color;
+                totalPoints.style.color = "red";
+                setTimeout(() => {
+                    totalPoints.style.color = tmpColor;
+                }, 3000);
+            }
+            setOpen(true);
+        }
+    }
+
+    return (
+        <div className="shop-main">
             <ul>
                 <li>
                     <ShopItem pts={70} buttext={"70 pts"} desc={"Skip one lecture"} className="shop-item"></ShopItem>
@@ -29,8 +59,28 @@ function Shop({childToParent}) {
                     <ShopItem pts={250} buttext={"250 pts"} desc={"15 Bearcat Card Dollars"} className="shop-item"></ShopItem>
                 </li>
             </ul>
-		</div>
-	);
+            <div className="shop-buy">
+                <span>Total: </span> <span id="shop-value">0</span> <span> points</span>
+                <Button id="shop-button" sx={{ width: "14vw", marginLeft: "15px" }} onClick={handleBuy} variant="contained">Buy</Button>
+
+            </div>
+
+
+            <Dialog
+                fullWidth="true"
+                maxWidth="md"
+                open={open}
+                onClose={handleClose}
+                sx={{ minHeight: "80vh" }}
+            >
+                <div className="shop-dialog">
+                    <p>
+                        {shopDialogString}
+                    </p>
+                </div>
+            </Dialog>
+        </div>
+    );
 }
 
 export default Shop;
--- src/shop/ShopItem.jsx
+++ src/shop/ShopItem.jsx
@@ -1,16 +1,23 @@
 import "./ShopItem.scss";
 import { useState } from 'react';
 
-function ShopItem({buttext, desc, pts}) {
+function ShopItem({ buttext, desc, pts }) {
     const [styleName, setStyleName] = useState("shop-button");
+    const [buttonActive, setButtonActive] = useState(false);
 
     const buttonSelected = () => {
         setStyleName(styleName === "shop-button-active" ? "shop-button" : "shop-button-active");
-        const currentPoints = document.getElementById("points")
-        currentPoints.textContent = parseInt(currentPoints.textContent, 10) - pts
+        const shopValue = document.getElementById("shop-value");
+        if (!buttonActive) {
+            setButtonActive(true);
+            shopValue.textContent = Number(shopValue.textContent) + pts;
+        } else {
+            setButtonActive(false);
+            shopValue.textContent = Number(shopValue.textContent) - pts;
+        }
     }
 
-	return (
+    return (
         <>
             <button className={styleName} onClick={buttonSelected}>{buttext}</button>
             <p className="shop-description">{desc}</p>
